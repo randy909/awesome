0828fa3715af2033681923acf0d85ecd803d83f2
Randy Hancock <randy.hancock@gmail.com>
2012-06-25 10:02:26 -0500

    Add Dmenu

--- rc.lua
+++ rc.lua
@@ -254,6 +254,15 @@ globalkeys = awful.util.table.join(
                   mypromptbox[mouse.screen].widget,
                   awful.util.eval, nil,
                   awful.util.getdir("cache") .. "/history_eval")
+              end),
+
+    -- My additions
+    awful.key({ modkey },            "d",
+              function ()
+                  awful.util.spawn("dmenu_run -i -p 'Run command:' -nb '" ..
+                  beautiful.bg_normal .. "' -nf '" .. beautiful.fg_normal ..
+                  "' -sb '" .. beautiful.bg_focus ..
+                  "' -sf '" .. beautiful.fg_focus .. "'")
               end)
 )
 
